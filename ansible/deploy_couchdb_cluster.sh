bcd71d9c02917a9169516ce180a35162fd40f95e
Steven5678231 <53145087+Steven5678231@users.noreply.github.com>
2021-05-10 13:38:48 +1000

    project init, cluster db setup

--- ansible/deploy_couchdb_cluster.sh
+++ ansible/deploy_couchdb_cluster.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-. ./hao-openrc.sh; ansible-playbook -vv deploy_couchdb_cluster.yaml -i inventory/customize_hosts.ini
+. ./unimelb-comp90024-2021-grp-10-openrc.sh; ansible-playbook -vv deploy_couchdb_cluster.yaml -i inventory/customize_hosts.ini
--- ansible/deploy_crawler.sh
+++ ansible/deploy_crawler.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-. ./hao-openrc.sh; ansible-playbook deploy_crawler.yaml -i inventory/customize_hosts.ini
+. ./unimelb-comp90024-2021-grp-10-openrc.sh; ansible-playbook deploy_crawler.yaml -i inventory/customize_hosts.ini
