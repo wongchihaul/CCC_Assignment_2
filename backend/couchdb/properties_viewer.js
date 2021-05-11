2f6de25d9a092325d8b2ccbea0812f1a59ec8674
Arya97 <34392034+Arya97@users.noreply.github.com>
2021-05-11 22:10:04 +1000

    tested views updated but keys are not displayed

--- backend/couchdb/properties_viewer.js
+++ backend/couchdb/properties_viewer.js
@@ -3,7 +3,7 @@ var aurin = couch.use('aurin');
 
 module.exports = propertiesViewer;
 async function propertiesViewer() {
-  var body = await aurin.view('viewTest', 'statByMonth')
+  var body = await aurin.view('viewTest', 'statTest')
   body.rows.forEach((doc) => {
     console.log('doc key', doc.key)
     console.log('doc value', doc.value);
--- backend/couchdb/views/view_for_properties.js
+++ backend/couchdb/views/view_for_properties.js
@@ -3,9 +3,9 @@ var aurin = couch.use('aurin');
 
 views = {
   "_id": "_design/viewTest",
-  // _rev is only required we the views need to be updated,
+  // _rev is only required when the views need to be updated,
   // hence it is not necessary when deploying the app
-  // "_rev": "13-f274c7e53bf76833e2597d66f16ea668",
+  // "_rev": "16-d010024fe220f4f986efe17db0e11f04",
   "views": {
     "counter": {
       "map": "function(doc) {\n if (doc.properties.for_rent_home_lease_minimumprice) {\n emit(doc._id, doc.properties.for_rent_home_lease_minimumprice); \n} \n}",
@@ -22,6 +22,10 @@ views = {
     "statByMonth": {
       "map": "function(doc) {\n if (doc.properties.for_rent_home_lease_minimumprice) {\n emit(doc.properties.datemonth, doc.properties.for_rent_home_lease_minimumprice); \n} \n}",
       "reduce": "_stats" 
+    },
+    "statTest": {
+      "map": "function(doc) {\n if (doc.properties.for_rent_home_lease_minimumprice) {\n emit(doc.properties.for_rent_home_lease_detailedposition45price, doc.properties.for_rent_home_lease_minimumprice); \n} \n}",
+      "reduce": "function(keys, values, rereduce) { return sum(values); }"
     }
   },
   "language": "javascript"
