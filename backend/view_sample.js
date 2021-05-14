0c8017cf592cde229d3bdb7a514b5733ae68a3cd
Steven5678231 <53145087+Steven5678231@users.noreply.github.com>
2021-05-14 21:11:31 +1000

    gitignore update

---
+++ backend/view_sample.js
@@ -0,0 +1,21 @@
+var get_income_by_city_and_year_and_month = {
+  row: [
+    { key: ["Brisbane", 2020, 1], value: 200 },
+    { key: ["Melbourne", 2020, 1], value: 1000 },
+    { key: ["Sydney", 2020, 1], value: 100 },
+    { key: ["Perth", 2020, 1], value: 100 },
+  ],
+};
+
+var get_emotion_score_by_city_and_year_and_day = {
+  row: [
+    { key: ["Brisbane", 2020, 1], value: 200 },
+    { key: ["Melbourne", 2020, 1], value: 1000 },
+    { key: ["Sydney", 2020, 1], value: 100 },
+    { key: ["Perth", 2020, 1], value: 100 },
+  ],
+};
+
+senario: [type1];
+data: [birth_and_death];
+by: [];
