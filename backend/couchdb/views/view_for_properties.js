26f634d5e1325757ee0b3b9c0b8680e2415e4e58
tanya <tanyali1025@gmail.com>
2021-05-14 20:52:28 +1000

    0514

--- backend/couchdb/views/view_for_properties.js
+++ backend/couchdb/views/view_for_properties.js
@@ -1,11 +1,11 @@
 var couch = require('../couchdb');
-var aurin = couch.use('aurin');
+var aurin = couch.use('test2');
 
 views = {
   "_id": "_design/viewTest",
   // _rev is only required when the views need to be updated,
   // hence it is not necessary when deploying the app
-  "_rev": "18-341ad26e0b4e4ccda0ed60f3fd45d588",
+  "_rev": "1-33e73062eca7b09a701bf16d74a9ca06",
   "views": {
     "counter": {
       "map": "function(doc) {\n if (doc.properties.for_rent_home_lease_minimumprice) {\n emit(doc._id, doc.properties.for_rent_home_lease_minimumprice); \n} \n}",
@@ -24,6 +24,10 @@ views = {
       "reduce": "_stats" 
     },
     "statTest": {
+      "map": "function(doc) {\n if (doc.properties.for_rent_home_lease_minimumprice) {\n emit(doc.properties.for_rent_home_lease_detailedposition45price, doc.properties.for_rent_home_lease_minimumprice); \n} \n}",
+      "reduce": "function(keys, values) { return sum(values); }"
+    },
+    "avg": {
       "map": "function(doc) {\n if (doc.properties.for_rent_home_lease_minimumprice) {\n emit(doc.properties.for_rent_home_lease_detailedposition45price, doc.properties.for_rent_home_lease_minimumprice); \n} \n}",
       "reduce": "function(keys, values) { return sum(values); }"
     }
