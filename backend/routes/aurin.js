e5561fca5864d855430aa70a310632fb5ff7aef6
Arya97 <34392034+Arya97@users.noreply.github.com>
2021-05-23 15:35:25 +1000

    add more aurin APIs

--- backend/routes/aurin.js
+++ backend/routes/aurin.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('api for aurin');
 });
-
+// router for labour market output
 router.get("/labour/info", (req, res) => {
   var labour_data = require('../../aurin/labour_market/output.json');
   const query_info = req.query;
@@ -12,26 +12,69 @@ router.get("/labour/info", (req, res) => {
   let isSummary = false; 
   isSummary = query_info.summary == "true"
 
-  if (query_info.state) {
-    const state = query_info.state;
-    res.json(filterForLabour(state, isSummary));
+  if (query_info.city) {
+    const city = query_info.city;
+    res.json(filterForLabour(city, isSummary));
   } else {
     res.json(labour_data);
   }
 
-  function filterForLabour(state, isSummary) {
+  function filterForLabour(city, isSummary) {
     if (isSummary) {
-      return labour_data[state].summary
+      return labour_data[city].summary
     } else {
-      return labour_data[state].details
+      return labour_data[city].details
     }
   }
 });
 
+// router for labour summary output
+router.get("/labour_summary/info", (req, res) => {
+  var labour_summary_data = require('../../aurin/labour_summary/output_sa4.json');
+  const query_info = req.query;
+
+  if (query_info.sa4) {
+    const sa4 = query_info.sa4;
+    res.json(labour_summary_data[sa4]);
+  } else {
+    res.json(labour_summary_data);
+  }
+});
+
+// router for migration output
 router.get("/migration/info", (req, res) => {
   var migration_data = require('../../aurin/migration/output.json');
   const query_info = req.query;
-  res.json(migration_data);
+
+  if (query_info.city) {
+    const city = query_info.city;
+    res.json(migration_data[city]);
+  } else {
+    res.json(migration_data);
+  }
+});
+
+// router for payroll output
+router.get("/payroll/info", (req, res) => {
+  var payroll_data = require('../../aurin/weekly_payroll/output.json');
+  const query_info = req.query;
+
+  if (query_info.state) {
+    const state = query_info.state;
+    if (query_info.suburb) {
+      const suburb = query_info.suburb;
+      if (query_info.month) {
+        const month = query_info.month;
+        res.json(payroll_data[state][suburb][month])
+      } else {
+        res.json(payroll_data[state][suburb])
+      }
+    } else {
+      res.json(payroll_data[state]);
+    }
+  } else {
+    res.json(payroll_data);
+  }
 });
 
 
