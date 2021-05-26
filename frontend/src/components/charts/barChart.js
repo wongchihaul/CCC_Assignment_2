90efe412baff073fc3c20884cbf3db28c3ac2a19
Steven5678231 <53145087+Steven5678231@users.noreply.github.com>
2021-05-26 17:18:17 +1000

    update endpoint

--- frontend/src/components/charts/barChart.js
+++ frontend/src/components/charts/barChart.js
@@ -1,31 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { Bar } from '@ant-design/charts';
+import React, { useState, useEffect } from "react";
+import { Bar } from "@ant-design/charts";
+const endpoint = "45.113.233.7";
 
-function BarChart(props){
-    const [data, setData] = useState()
-    
-    useEffect(() => {
-        asyncFetchData(props.city);
-    }, [props.city]);
-    const asyncFetchData = (_city) => {
-        fetch(`http://127.0.0.1:3001/aurin/projection/info?city=${_city}`)
-        .then(res=> res.json())
-        .then(json=>{
-            setData(json.sort((a,b)=>(b.percentage - a.percentage)))
-        })
-    }
+function BarChart(props) {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    asyncFetchData(props.city);
+  }, [props.city]);
+  const asyncFetchData = (_city) => {
+    fetch(`http://${endpoint}:3001/aurin/projection/info?city=${_city}`)
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json.sort((a, b) => b.percentage - a.percentage));
+      });
+  };
   var config = {
     data: data,
-    xField: 'percentage',
-    yField: 'name',
-    seriesField: 'name',
-    legend: { position: 'top-left' },
+    xField: "percentage",
+    yField: "name",
+    seriesField: "name",
+    legend: { position: "top-left" },
   };
-  return (
-      <>
-      {data && (<Bar {...config} />)}
-      </>
-  )
+  return <>{data && <Bar {...config} />}</>;
 }
 
 export default BarChart;
